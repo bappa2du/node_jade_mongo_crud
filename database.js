f033406d9c642cbfdccc1cc425adb4b7c41a1d8e
bappa <iftakharul.alam@live.com>
2015-07-13 18:24:18 +0600

    project init

---
+++ database.js
@@ -0,0 +1,10 @@
+var mongoose = require('mongoose');
+var Schema   = mongoose.Schema;
+
+var Comment = new Schema({
+    title : String,
+});
+
+mongoose.model('comments', Comment);
+
+mongoose.connect('mongodb://root:root@ds029821.mongolab.com:29821/userstory');
